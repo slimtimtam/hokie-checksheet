3fce567db198087257e3da1a1594f3c2e962df4c
Alex Bochel <alexbochel@Alexs-MacBook-Air.local>
2018-07-25 20:02:59 -0400

    Added classes for draggable item, item container and semester container with dragging implementation but no drag release

---
+++ src/react/DraggableClassItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { DragSource } from "../../node_modules/react-dnd";
+import "../App.css"
+
+const draggableClassItemSource = {
+  beginDrag(props) {
+    return props.item;
+  },
+  endDrag(props, monitor, component) {
+    return props.handleDrop(props.item.id);
+  }
+}
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  }
+}
+
+class DraggableClassItem extends React.Component
+{
+  render() {
+    const { isDragging, connectDragSource, item } = this.props
+
+    return connectDragSource(
+      <div className="draggable-class-item-source">
+        <span>{item.name}</span>
+      </div>
+    );
+  }
+}
+
+export default DragSource('draggableClassItem', draggableClassItemSource, collect)(DraggableClassItem)
